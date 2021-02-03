666396d05b245411502c670c85e957e50c2ac80e
Tim Miller <dutsmiller@gmail.com>
2021-02-03 18:11:09 -0500

    initial copy (#1)
    * initial copy
    
    * update README
    
    * update README
    
    * update README
    
    Co-authored-by: Tim Miller <tim.miller@risk.lexisnexis.com>

---
+++ variables.tf
@@ -0,0 +1,118 @@
+##
+# Required parameters
+##
+variable "name" {
+  description = "Storage account name."
+  type        = string
+  default     = null
+}
+
+variable "location" {
+  description = "Specifies the supported Azure location to MySQL server resource"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "name of the resource group to create the resource"
+  type        = string
+}
+
+variable "names" {
+  description = "names to be applied to resources"
+  type        = map(string)
+}
+
+variable "tags" {
+  description = "tags to be applied to resources"
+  type        = map(string)
+}
+
+variable "account_kind" {
+    description = "Defines the Kind of account. Valid options are BlobStorage, BlockBlobStorage, FileStorage, Storage and StorageV2"
+    type        = string
+}
+
+variable "account_tier" {
+    description = "Defines the Tier to use for this storage account (Standard or Premium)."
+    type        = string
+    default     = null
+}
+
+variable "replication_type" {
+    description = "Storage account replication type - i.e. LRS, GRS, RAGRS, ZRS, GZRS, RAGZRS."
+    type        = string
+}
+
+variable "access_tier" {
+    description = "Defines the access tier for BlobStorage, FileStorage and StorageV2 accounts (Hot or Cold)."
+    type        = string
+    default     = "Hot"
+}
+
+variable "enable_large_file_share" {
+  description = "Enable Large File Share."
+  type        = bool
+  default     = false
+}
+
+variable "enable_hns" {
+  description = "Enable Hierarchical Namespace (can be used with Azure Data Lake Storage Gen 2)."	
+  type        = bool
+  default     = false
+}
+
+variable "enable_https_traffic_only" {
+  description = "Forces HTTPS if enabled."
+  type        = bool
+  default     = true
+}
+
+variable "min_tls_version" {
+  description = "The minimum supported TLS version for the storage account."
+  type        = string
+  default     = "TLS1_2"
+}
+
+variable "allow_blob_public_access" {
+ description = "Allow or disallow public access to all blobs or containers in the storage account."
+ type        = bool
+ default     = false
+}
+
+# Note: make sure to include the IP address of the host from where "terraform" command is executed to allow for access to the storage
+# Otherwise, creating container inside the storage or any access attempt will be denied.
+variable "access_list" {
+  description = "Map of CIDRs Storage Account access."
+  type        = map(string)
+  default     = {}
+}
+
+variable "service_endpoints" {
+  description = "Creates a virtual network rule in the subnet_id (values are virtual network subnet ids)."
+  type        = map(string)
+  default     = {}
+}
+
+variable "traffic_bypass" {
+  description = "Specifies whether traffic is bypassed for Logging/Metrics/AzureServices. Valid options are any combination of Logging, Metrics, AzureServices, or None."
+  type        = list(string)
+  default     = ["None"]
+}
+
+variable "blob_delete_retention_days" {
+    description = "Retention days for deleted blob. Valid value is between 1 and 365."
+    type        = number
+    default     = 7
+}
+
+variable "blob_cors" {
+  description = "blob service cors rules:  https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/storage_account#cors_rule"
+  type        = map(object({
+                  allowed_headers    = list(string)
+                  allowed_methods    = list(string)
+                  allowed_origins    = list(string)
+                  exposed_headers    = list(string)
+                  max_age_in_seconds = number
+                }))
+  default     = null
+}
