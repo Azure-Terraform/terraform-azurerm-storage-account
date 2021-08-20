16e70a1fbcc9d23ab3aacca3b2006ab9b3659bd1
waylew-lexis <86667349+waylew-lexis@users.noreply.github.com>
2021-08-20 11:08:59 -0400

    updating versions.tf to be less restrictive on provider versions

--- versions.tf
+++ versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 2.72"
+      version = ">= 2.7"
     }
     random = {
       source= "hashicorp/random"
