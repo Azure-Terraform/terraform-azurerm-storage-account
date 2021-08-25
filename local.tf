7fa68213c380b4a6962b0483173eee152de17bc6
Tim Miller <tim.miller@risk.lexisnexis.com>
2021-08-25 14:42:48 -0400

    fix validation

--- local.tf
+++ local.tf
@@ -7,10 +7,10 @@ locals {
   validate_static_website = ((var.enable_static_website && var.account_kind == "BlockBlobStorage" || var.account_kind == "StorageV2") ?
   true : file("ERROR: Account kind must be BlockBlobStorage or StorageV2 when enabling static website"))
 
-  validate_nfsv3 = ((var.nfsv3_enabled && var.enable_hns) ?
+  validate_nfsv3 = (!var.nfsv3_enabled || (var.nfsv3_enabled && var.enable_hns) ?
   true : file("ERROR: NFS V3 can only be enabled when Hierarchical Namespaces are enabled"))
 
-  validate_nfsv3_network_rules = ((var.nfsv3_enabled && lower(var.default_network_rule) == "deny") ?
+  validate_nfsv3_network_rules = (!var.nfsv3_enabled || (var.nfsv3_enabled && lower(var.default_network_rule) == "deny") ?
   true : file("ERROR: Default network rule must be Deny when using NFS V3"))
 
   validate_network_rules = ((lower(var.default_network_rule) == "deny" && var.access_list == {} && var.service_endpoints == {})  ?
