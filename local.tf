a64c0ee4b4acefae9fa2cb432eeae03b1201ea04
Joshua Hiscocks <joshua.hiscocks@lexisnexisrisk.com>
2021-08-25 16:54:51 +0100

    Added NFSv3 Validation

--- local.tf
+++ local.tf
@@ -6,4 +6,7 @@ locals {
 
   validate_static_website = ((var.enable_static_website && var.account_kind == "BlockBlobStorage" || var.account_kind == "StorageV2") ?
   true : file("ERROR: Account kind must be BlockBlobStorage or StorageV2 when enabling static website"))
+
+  validate_nfsv3 = ((var.nfsv3_enabled && !var.enable_hns) ?
+  true : file("ERROR: NFS V3 can only be enabled when Hierarchical Namespaces are enabled"))
 }
