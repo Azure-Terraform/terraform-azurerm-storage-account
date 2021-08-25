ec934a11020e2706dd699d8abe49c0ebc8bdafa7
Joshua Hiscocks <joshua.hiscocks@lexisnexisrisk.com>
2021-08-25 17:49:29 +0100

    Default Network Rule support added

--- variables.tf
+++ variables.tf
@@ -154,7 +154,7 @@ variable "nfsv3_enabled" {
 }
 
 variable "default_network_rule" {
-  description = ""
+  description = "Specifies the default action of allow or deny when no other network rules match"
   type        = string
   default     = "Deny"
 
