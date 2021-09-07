aa00afe672dbdb6a43b9e5ffcc726ae7d72bb699
Neil Wilson <77080362+wilsonnm02@users.noreply.github.com>
2021-09-07 12:30:59 -0500

    Enable CORS for StorageV2 SKU and correct logic issue with validate_static_website evaluation (#33)
    * Adds primary and secondary blob hosts to output
    
    Adds primary and secondary blob hosts to output from the storage account. These values are required downstream if you are setting up a cdn endpoint.
    
    * Adds tags to the storage_account
    
    Adds tags to the storage_account resource
    
    * Trying to correct logic for enable_static_website flag
    
    Trying to correct logic for enable_static_website flag. If enable_static_website = false, then validate_static_website should be allowed to evaluate to false instead of throwing an error
    
    * Enable CORS policy application for StorageV2
    
    Enable CORS policy application for StorageV2. Previously only BlockBlobStorage was allowed which conflicts with our need for RAGRS for CDN Storage Accounts
    
    * Update main.tf
    
    * Revert of access_list
    
    Revert of access_list

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ resource "azurerm_storage_account" "sa" {
   }
 
   dynamic "blob_properties" {
-    for_each = (var.account_kind == "BlockBlobStorage" ? [1] : [])
+    for_each = ((var.account_kind == "BlockBlobStorage" || var.account_kind == "StorageV2") ? [1] : [])
     content {
       dynamic "delete_retention_policy" {
         for_each = (var.blob_delete_retention_days == null ? [] : [1])
