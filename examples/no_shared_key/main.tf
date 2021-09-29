21e4cd298b1b1a0ab92e0ddf5504a831f3998371
wayne lewalski <86667349+waylew-lexis@users.noreply.github.com>
2021-09-29 11:26:43 -0400

    added example on how to create a storage account when shared access key is disabled (#37)
    * adding example  to run tf with shared key disabled
    
    * updating example
    
    * updating example

---
+++ examples/no_shared_key/main.tf
@@ -0,0 +1,28 @@
+provider "azurerm" {
+  
+  # by default teraform uses the shared access key to access the storage account.
+  # to have terraform, use azure ad instead, set this property to true
+  storage_use_azuread = true
+  
+  features {}
+}
+
+module "storage_account" {
+  source = "../../"
+
+  resource_group_name = module.resource_group.name
+  location            = module.resource_group.location
+  names               = module.metadata.names
+  tags                = module.metadata.tags
+
+  account_kind     = "StorageV2"
+  replication_type = "LRS"
+  account_tier     = "Standard"
+
+  shared_access_key_enabled = false
+
+
+  service_endpoints = {
+    "iaas-outbound" = module.virtual_network.subnet["iaas-outbound"].id
+  }
+}
