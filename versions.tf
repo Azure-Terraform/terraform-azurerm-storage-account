dc7cd08d7f6cfc6f7310af2ffb1c6759221e6ac8
Vishnu Gantam <91073177+gantvi01@users.noreply.github.com>
2022-12-09 15:16:32 -0600

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,14 +1,10 @@
 terraform {
-  required_version = "~> 1.3"
+  required_version = ">= 1.3.3, != 1.3.4"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "> 3.6"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 3.1"
+      version = ">= 3.31.0"
     }
   }
 }
