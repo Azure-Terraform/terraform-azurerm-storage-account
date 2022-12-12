6fd1ee41e7e8caccd63190ba8b3f12c4a0fd6d2e
Vishnu Gantam <91073177+gantvi01@users.noreply.github.com>
2022-12-12 14:43:36 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -187,7 +187,7 @@ variable "container_delete_retention_days" {
 variable "public_network_access_enabled" {
   description = "public access to set to flase for SA"
   type = bool
-  default = false
+  default = true
 }
 
 variable "sftp_enabled" {
